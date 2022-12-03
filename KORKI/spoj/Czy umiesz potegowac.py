065efe2fd3fa7c98acc7ffadd816df89f8ce99e6
Mikolaj <73663166+MiQiN420@users.noreply.github.com>
2022-12-03 11:43:05 +0100

    Add files via upload

---
+++ KORKI/spoj/Czy umiesz potegowac.py
@@ -0,0 +1,15 @@
+
+"""
+czy umiesz potegowac:
+a ^ b = a * a * ... b times
+podajemy a oraz b
+"""
+tests = int(input("Ilosc prob: "))
+for _ in range(tests):
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    wynik = a
+    for _ in range(b - 1):
+        wynik *= a
+    print(wynik % 10)
